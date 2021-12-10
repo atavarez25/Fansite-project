bf75f4e04116491087302cfb66f66893e73e46f5
atavarez25 <atavarez24@aoiths.org>
2021-12-10 09:45:19 -0500

    a

---
+++ load.js
@@ -0,0 +1,23 @@
+//
+var html = `	<img id="Banner" alt="Banner" src="#">
+ 	<div id="Navigation">
+ 		<a href="#"> About </a>
+ 		<a href="#"> Discography </a>
+ 		<a href="#"> Multimedia </a>
+ 		<a href="#"> References </a>
+ 	</div>
+
+ 	 	<footer>
+        aaa
+ 		<hr>
+        aaa
+        <hr>
+        aaa
+        <hr>
+        aaa
+
+ 	</footer> 
+
+`
+
+document.write(html);
