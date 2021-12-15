6816d5614529bb884f02e976776389236c22da4a
atavarez25 <atavarez24@aoiths.org>
2021-12-15 09:12:28 -0500

    navigation and about page

--- load.js
+++ load.js
@@ -1,8 +1,8 @@
 //
 var html = `	<img id="Banner" alt="Banner" src="#">
  	<div id="Navigation">
- 		<a href="#"> About </a>
- 		<a href="#"> Discography </a>
+ 		<a href="index.html"> About </a>
+ 		<a href="Discography.html"> Discography </a>
  		<a href="#"> Multimedia </a>
  		<a href="#"> References </a>
  	</div>
