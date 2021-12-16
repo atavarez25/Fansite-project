7a2d0c176cac833bffbf64021ece2eb257d8d6e1
atavarez25 <atavarez24@aoiths.org>
2021-12-16 09:30:02 -0500

    references page

--- load.js
+++ load.js
@@ -3,8 +3,8 @@ var html = `	<img id="Banner" alt="Banner" src="#">
  	<div id="Navigation">
  		<a href="index.html"> About </a>
  		<a href="Discography.html"> Discography </a>
- 		<a href="#"> Multimedia </a>
- 		<a href="#"> References </a>
+ 		<a href="?"> Multimedia </a>
+ 		<a href="reference.html"> References </a>
  	</div>
 
  	 	<footer>
